fc842a028ec9017e3f8dd5942e3927a815d6e08d
Todd Gore <toddgore@Todds-MacBook-Pro.local>
2018-06-01 11:33:19 -0600

    Still building

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
+// const cors = require('cors')
+
+require('dotenv').config();
+
 
 const app = express();
 app.use(bodyParser.json());
+// app.use(cors());
 
-const port = 3047;
 
+
+
+massive(process.env.CONNECTION_STRING).then(
+    dbInstance => app.set('db', dbInstance));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Started server on port ', port);
 });
---
+++ src/route.js
@@ -0,0 +1,10 @@
+import { Route, Switch } from 'react-router-dom'
+import Dashboard from "./components/Dashboard/Dashboard";
+import Wizard from "./components/Wizard/Wizard";
+
+export default (
+    <Switch>
+        <Route path='/' component={Dashboard} exact />
+        <Route path='/wizard' component={Wizard} />
+    </Switch>
+)
